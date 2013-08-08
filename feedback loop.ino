ac59d1b07eefd13d49a705dfdb0862124351bdc2
youtubeoreo <shrek2357@gmail.com>
2013-08-08 11:50:45 -0700

    Create feedback loop.ino

---
+++ feedback loop.ino
@@ -0,0 +1,88 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+void loop()
+{
+  if(digitalRead(13)==HIGH)
+  {
+    digitalRead(9,LOW);
+    digitalRead(3,LOW);
+  }
+  else if((digitalRead(13)==LOW)
+  {
+    digitalRead(9,HIGH);
+    digitalRead(3,LOW);
+  }
+  else if ((digitalRead(9)==LOW)
+  {
+  digitalRead(3,HIGH);
+  digitalRead(13,LOW);
+}
+   else((digitalRead(3)==LOW) 
+   {
+     digitalRead(13,HIGH);
+     digitalRead(9,LOW);
+   }
+   
+   void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+void loop()
+{
+  if(digitalRead(13)==HIGH)
+  {
+    digitalRead(9,LOW);
+    digitalRead(3,LOW);
+  }
+  else if((digitalRead(13)==LOW)
+  {
+    digitalRead(9,HIGH);
+    digitalRead(3,LOW);
+  }
+  else if ((digitalRead(9)==LOW)
+  {
+  digitalRead(3,HIGH);
+  digitalRead(13,LOW);
+}
+   else((digitalRead(3)==LOW) 
+   {
+     digitalRead(13,HIGH);
+     digitalRead(9,LOW);
+   }
+   
+   void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+void loop()
+{
+  if(digitalRead(13)==HIGH)
+  {
+    digitalRead(9,LOW);
+    digitalRead(3,LOW);
+  }
+  else if((digitalRead(13)==LOW)
+  {
+    digitalRead(9,HIGH);
+    digitalRead(3,LOW);
+  }
+  else if ((digitalRead(9)==LOW)
+  {
+  digitalRead(3,HIGH);
+  digitalRead(13,LOW);
+}
+   else((digitalRead(3)==LOW) 
+   {
+     digitalRead(13,HIGH);
+     digitalRead(9,LOW);
+   }
+   
+   
